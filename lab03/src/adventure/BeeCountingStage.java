49434426702aa2cd9fd2827b91bde86381c64d46
桓宪柱 <2754741241@qq.com>
2026-02-18 09:50:08 +0800

    lab 03: BeeCountingStage

--- lab03/src/adventure/BeeCountingStage.java
+++ lab03/src/adventure/BeeCountingStage.java
@@ -3,6 +3,8 @@ package adventure;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,7 +13,7 @@ public class BeeCountingStage implements AdventureStage {
 
     private final In in;
     private final Map<String, AdventureStage> responses;
-    private List<String> input;
+    private List<String> input = new ArrayList<>();
 
     public BeeCountingStage(In in) {
         this.in = in;
@@ -83,7 +85,7 @@ public class BeeCountingStage implements AdventureStage {
      */
     private int sumInput() {
         int sum = 0;
-        for (int i = 0; i <= this.input.size(); i++) {
+        for (int i = 0; i < this.input.size(); i++) {
             sum += Integer.parseInt(this.input.get(i));
         }
         return sum;
